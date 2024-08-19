93b6b0d6e6f539327e8a25ede92596523b46a463
ZaynOssama <oussamazazou@iesmontsia.org>
2024-08-19 20:10:41 +0200

    Set up the project

---
+++ src/views/home.jsx
@@ -0,0 +1,18 @@
+import {React} from 'react';
+
+function home() {
+  return (
+    <div className="home">
+        <div className='d-flex justify-content-center center'>
+            <div>
+                Hello
+            </div>
+            <div className='ps-2 fw-bold'>
+                World!
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default home;
