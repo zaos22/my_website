9c397b0b05490cd1aa1329fff66905b6dc2f7273
ZaynOssama <oussamazazou@iesmontsia.org>
2024-08-19 23:53:34 +0200

    Still working on the interface

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,5 +1,7 @@
 import { React } from 'react';
 import defaultIcon from '../img/default-profile-icon.png';
+import { ImQuotesLeft, ImQuotesRight } from "react-icons/im";
+import ProgressBar from '../components/progressBar';
 
 function home() {
     const url = 'https://zaos.website.vercel.app.com';
@@ -8,22 +10,37 @@ function home() {
         <div className="home">
             <div className='center'>
                 <div className='d-flex justify-content-start'>
-                    <div className='dot1' />
-                    <div className='dot2 ms-1' />
-                    <div className='dot3 ms-1' />
+                    <div className='dot1 mt-2' />
+                    <div className='dot2 mt-2 ms-1' />
+                    <div className='dot3 mt-2 ms-1' />
 
-                    <input className='input-top-bar ms-3' type="text" defaultValue={url} />
+                    <input
+                        className='input-top-bar ms-3'
+                        type="text"
+                        defaultValue={url}
+                        disabled={true}
+                    />
                 </div>
 
-                <div className='d-flex justify-content-center'>
-                    <div className='image-icon'>
-                        {/* <img src={defaultIcon} alt="Default Icon" /> */}
+                <div className='d-flex justify-content-center mt-5 mb-5'>
+                    <div className='image-icon' style={{ with: '40%' }}>
+                        <img src={defaultIcon} alt="Default Icon" />
                     </div>
-                    <div>
-                        Hello
+                </div>
+                <div>
+                    <ProgressBar value={50}/>
+                </div>
+                <div className='d-flex justify-content-center mt-5 mb-5'>
+                    <div className='ms-5'>
+                        <ImQuotesLeft color='black' size={50} />
                     </div>
-                    <div className='ps-2 fw-bold'>
-                        World!
+                    <div className='ms-5 me-5 fw-bold text-center d-flex align-items-center justify-content-center' style={{ width: '60%' }}>
+                        <span className='ms-2 me-2'>
+                            I'm Oussama, a passionate web developer with a strong command of full-stack programming. My objective is to turn ideas into minimalist websites, but at the same time that captivate and engage users. Let's build something simple, but amazing together!
+                        </span>
+                    </div>
+                    <div className='me-5' style={{ position: 'absolute', bottom: '30px', right: '60px' }}>
+                        <ImQuotesRight color='black' size={50} />
                     </div>
                 </div>
             </div>
